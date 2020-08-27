a427a280a220172e9da39949dd8f1483628ed28c
rodrigo-marcolino <rodrigo-marcolino@users.noreply.github.com>
2020-08-27 16:52:35 +1200

    final

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -20,7 +20,6 @@ namespace GradeBook.GradeBooks
             Name = name;
             Students = new List<Student>();
         }
-        public Type BaseGradeBook
 
         public void AddStudent(Student student)
         {
